6c07b104d4568fa02d85a23d39dc43e1a3a08325
Rhodri Davies <32064462+RhodriDavies7@users.noreply.github.com>
2020-11-27 17:06:03 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var i = 0;
+var quotes = ['Burning through NCIS firewall',
+              'Launching Cyber Nuke',
+              'Nmapping the script to view the cipher text',
+              'Bettering the grasp on python 6 malware encryption',
+              'Connecting the virtual TCP to the open source sensor']; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+var j = Math.floor(Math.random() * quotes.length);
+
+function typeWriter() {
+  if (i < quotes[j].length) {
+    document.getElementById("quotes").innerHTML += quotes[j].charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
